d92c63c62b46317302ed190030bf4953f26baea6
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-05-10 02:53:44 +0530

    New

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from "@/components/ui/button";
 
 export default function Home () {
   return (
-    <Button variant="destructive" >Click Me </Button>
+    <Button variant="destructive" size="lg">Click Me </Button>
   )
 }
